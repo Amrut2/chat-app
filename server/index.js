de66f2893af66780113e3e71c4fe1ceb48139458
Puyed <amrut-rajesh.puyed@capgemini.com>
2023-11-03 11:52:43 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const { Server }  = require("socket.io")
+const cors = require("cors")
+
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const  io = new Server(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        methods:['GET', 'POST']
+    }
+})
+
+
+io.on('connection', (socket)=>{
+
+    console.log(`User Connected: ${socket.id}`)
+    
+    socket.on('send_message', (data) =>{
+        socket.broadcast.emit('recieve_message', data)
+
+    })
+
+})
+
+server.listen(4000, ()=>{
+    console.log(`Server started on port 4000`)
+})
